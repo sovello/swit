8f2b9b9a7426f2a622ca9a7afa8b31e08022daad
Brandon Bickford <bickfordb@gmail.com>
2012-12-19 10:31:05 -0800

    Miscellaneous changes
    Add a some docstrings
    Add an id to the list of cadres returned for "/api/1.0/cadres"

--- src/web/sb/healthworker/views.py
+++ src/web/sb/healthworker/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 from sb.healthworker import models
 
 def _to_json_default(an_object):
+  "JSON converter function to convert dates and datetimes to UTC timestamps"
   if an_object is None:
     return None
   elif isinstance(an_object, datetime.datetime):
@@ -23,21 +24,31 @@ def _to_json_default(an_object):
     raise TypeError(type(an_object))
 
 def _to_json_response(data, status=200):
+  """Convert 'data' to a JSON response.
+
+  This serializes 'data' to JSON and returns an HTTP response with the
+  "Content-Type" header set to "application/json".
+
+  Arguments:
+  data --- any, a value that json.dumps can marshal
+
+  Returns
+  django.http.HttpResponse
+  """
   return HttpResponse(json.dumps(data, default=_to_json_default),
                       status=status,
                       content_type="application/json")
-
-def _to_timestamp(a_date_time):
-  "Convert a date time object to a timestamp"
-  return time.mktime(a_date_time.utctimetuple()) if a_date_time else None
 
 def _cadre_to_dictionary(cadre):
   "Convert a Cadre to a dictionary suitable for JSON encoding"
   return {"created_at": cadre.created_at,
           "updated_at": cadre.updated_at,
+          "id": cadre.id,
           "title": cadre.title}
 
 def on_cadre_index(request):
+  """Get a list of cadres"""
   cadres = models.Cadre.objects.all()
   return _to_json_response(map(_cadre_to_dictionary, cadres))
+
 
