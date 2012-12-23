13773c8d3f673e02b53108bd5e2d50ab2b3a3d77
Brandon Bickford <bickfordb@gmail.com>
2012-12-23 10:45:10 -0800

    Add model admin to regions

--- src/web/sb/healthworker/admin.py
+++ src/web/sb/healthworker/admin.py
@@ -6,6 +6,18 @@ admin.site.register(models.FacilityType)
 admin.site.register(models.HealthWorker)
 admin.site.register(models.MCTRegistration)
 admin.site.register(models.MCTPayroll)
-admin.site.register(models.Region)
+
+class RegionAdmin(admin.ModelAdmin):
+
+  list_display = ["title", "type", "parent_region", "created_at", "updated_at", "subregions"]
+  search_fields = ["title"]
+
+  def parent_title(self, o):
+    return o.parent_region.title
+
+  def subregions(self, o):
+    return ', '.join(m.title for m in models.Region.objects.filter(parent_region=o).all())
+
+admin.site.register(models.Region, RegionAdmin)
 admin.site.register(models.RegionType)
 admin.site.register(models.Specialty)
