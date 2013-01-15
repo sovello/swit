8bbc47b5da5e520dac83da64c15ecadc53ff52d5
Brandon Bickford <bickfordb@gmail.com>
2013-01-14 21:02:24 -0800

    fix for reals

---
+++ src/web/sb/healthworker/migrations/0011_specialty_unicode2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+from django.db import transaction
+from sb.healthworker import models
+
+class Migration(SchemaMigration):
+
+  def forwards(self, orm):
+    with transaction.commit_on_success():
+      for s in models.Specialty.objects.all():
+        s.title = s.title.replace(u'\u200e', '')
+        s.save()
+
+  def backwards(self, orm):
+    pass
+
