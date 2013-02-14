8b6edfba5890f6a90967a19ca6c9a0702af1b8d6
Brandon Bickford <bickfordb@gmail.com>
2013-02-14 09:33:31 -0800

    add added_to_closed_user_group col

---
+++ src/web/sb/healthworker/migrations/0024_healthworker_added_to_closed_user_group_at.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+      db.add_column('healthworker_healthworker', 'added_to_closed_user_group_at', models.DateTimeField(null=True, default=None, blank=True))
+
+    def backwards(self, orm):
+      pass
+
