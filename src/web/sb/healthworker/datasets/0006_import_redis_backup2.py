c3d4490f7433ccc6e88a98bfd20a9cf79a613a91
Matt Olson <matt@mattolson.com>
2013-10-06 13:19:18 -0700

    Add redis backup import
    -- Refactor redis import functionality so we can reuse it
    -- Add import for latest redis backup

--- src/web/sb/healthworker/datasets/0006_import_redis_backup2.py
+++ src/web/sb/healthworker/datasets/0006_import_redis_backup2.py
@@ -2,7 +2,7 @@ from sb.healthworker.datasets import _redis_import
 from sb.healthworker.datasets import _helpers
 
 def run():
-  _redis_import.import_redis_backup(_helpers.get_path('kv-backup-20130715.json'))
+  _redis_import.import_redis_backup(_helpers.get_path('kv-backup-20131005.json'))
 
 if __name__ == '__main__':
   run()
